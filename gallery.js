e6c5ff93c55164f0d04b421cc7eafb4a45e0c002
rich21caaya <67361950+rich21caaya@users.noreply.github.com>
2021-07-17 18:04:36 +0800

    Create gallery.js

---
+++ gallery.js
@@ -0,0 +1,11 @@
+function upDate(previewPic){
+    var src = previewPic.getAttribute( "src" );
+    document.getElementById('image').style.backgroundImage = "url('" + src + "')";
+    document.getElementById('image').innerHTML = previewPic.alt;
+}
+
+function unDo(){
+    var text = "Hover over an image below to display here.";
+    document.getElementById('image').style.backgroundImage = "url('')";
+    document.getElementById('image').innerHTML= text;
+}
